0dbaaa740658da96f3f50bc9a74fce57de677484
Peter Elliott <p.elliott332@gmail.com>
2018-04-12 09:48:48 -0600

    wrote password generator

---
+++ src/password.c
@@ -0,0 +1,60 @@
+#include "password.h"
+#include <openssl/sha.h>
+#include <openssl/bio.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+/*
+    generate the password for a certain system and account
+*/
+void get_password(char *system, char *account, char *master_pass, int secret_fd) {
+    
+    char id_hash[SHA_256_DIGEST_LENGTH];
+    char pass_hash[SHA_256_DIGEST_LENGTH];
+    char secret_hash[SHA_256_DIGEST_LENGTH];
+
+    SHA256_CTX sha256;
+
+    // calculate the identity hash
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, system, strlen(system));
+    SHA256_Update(&sha256, account, strlen(account));
+    SHA256_Final(id_hash, &sha256);
+
+    // calculate the password hash
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, master_pass, strlen(master_pass));
+    SHA256_Final(pass_hash, &sha256);
+
+    // calculate the secret hash
+    SHA256_Init(&sha256);
+    char secret_read[SECRET_BLOCK_READ];
+    int bytes_read;
+    do {
+        bytes_read = read(secret_fd, secreat_read, SECRET_BLOCK_READ); 
+        // NOTE: one update will have size 0
+        SHA256_Update(&sha256, secret, strlen(secret));
+    } while(bytes_read);
+    SHA256_Final(secret_hash, &sha256);
+
+    // generate the output hash
+    char out_hash[SHA_256_DIGEST_LENGTH];
+    SHA_256_Init(&sha256);
+    SHA_256_Update(&sha256, id_hash, SHA_256_DIGEST_LENGTH);
+    SHA_256_Update(&sha256, pass_hash, SHA_256_DIGEST_LENGTH);
+    SHA_256_Update(&sha256, secret_hash, SHA_256_DIGEST_LENGTH);
+    SHA_256_Final(out_hash, &sha256);
+
+    // output the password
+    BIO *bio
+    BIO *b64;
+    b64 = BIO_new(BIO_f_base64());
+    bio = BIO_new_fp(stdout, BIO_NOCLOSE);
+    BIO_push(b64, bio);
+    BIO_write(b64, out_hash, PW_ENTROPY);
+    BIO_flush(b64);
+
+    BIO_free_all(b64);
+    putchar('\n');
+}
---
+++ src/password.h
@@ -0,0 +1,9 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+#define SECRET_BLOCK_READ 1024
+#define PW_ENTROPY 128
+
+void get_password(char *system, char *account, char *master_pass, int secret_fd);
+
+#endif
