c9cbb973af1150ae96c9ba56dc688885d9696a84
Peter Elliott <p.elliott332@gmail.com>
2018-04-13 10:45:07 -0600

    added reference implemntation for testing

---
+++ test/reference.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# input $1=target $2=password $3=file
+
+echo -n "$(echo -n $1 | sha256sum -b | xxd -r -p -)$(echo -n $2 | sha256sum -b | xxd -r -p -)$(sha256sum -b $3 | xxd -r -p -)"\
+        | sha256sum -b | xxd -r -p - | head --bytes=16 | base64
